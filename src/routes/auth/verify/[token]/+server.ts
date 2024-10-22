aed276902bf54f2012f6bc80e603b5417697c474
Rishab Sharma <77726912+donnutdev@users.noreply.github.com>
2024-10-22 20:52:39 +0530

    Update +server.ts
    changed redirect after verify

--- src/routes/auth/verify/[token]/+server.ts
+++ src/routes/auth/verify/[token]/+server.ts
@@ -4,5 +4,5 @@ export const GET = ({ locals, params }) => {
     const token = params.token
     locals.pb.collection('users').confirmVerification(token)
 
-    return redirect(303, '/rank')
+    return redirect(303, '/rate')
 };
